33c99a20b460e8bf4ee5416d7496f83d11559542
Adam Johns <adamjohns2006@icloud.com>
2026-07-02 12:16:26 +0100

    feat: add SQLAlchemy model foundation

---
+++ app/models/base.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import DateTime, MetaData, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=convention)
+
+
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[int_pk]
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
